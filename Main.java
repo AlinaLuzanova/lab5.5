a98203d01ae120d3824fbcefdba1d91b3918147a
AlinaLuzanova <alinaluzanova@gmali.com>
2022-03-16 15:13:26 +0300

    test1

--- Main.java
+++ Main.java
@@ -2,8 +2,33 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Commands commands = new Commands(new CollectionManager(System.getenv("Collman_Path")));
-        commander.interactiveMod();
-    }
+        FileM fileM = new FileM();
 
+        TreeSet<Product> products = fileM.readCSV();
+
+        CollectionManager collectionManager = new CollectionManager(products);
+        System.out.println("Добро пожаловать!");
+        System.out.println("Напечатайте  help, чтобы начать");
+        InputChecker inputChecke = new InputChecker();
+        ProductAsker commandAsker = new ProductAsker();
+        CommandManager commandManager = new CommandManager(
+                new help(),
+                new info(),
+                new show (),
+                new add (),
+                new update_id (),
+                new remove_by_id(),
+                new clear(),
+                new execute_script (),
+                new exit (),
+                new remove_first(),
+                new shuffle(),
+                new history (),
+                new sum_of_annual_turnover (),
+                new count_less_than_postal_address (),
+                new filter_by_employees_count () );
+        Commander commander = new Commander(commandManager, new Scanner(System.in), fileM);
+        commander.interactiveMode();
+
+    }
 }
